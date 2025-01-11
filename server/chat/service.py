0572e4705453400bc0a4e6a1270702f90d9da606
Andrew Wang <awwang3@uci.edu>
2025-01-11 10:43:23 -0800

    gitignore

--- server/chat/service.py
+++ server/chat/service.py
@@ -4,14 +4,16 @@ import json
 
 load_dotenv("../.env")
 
+
 class ChatService:
     def __init__(self):
         self.client = OpenAI()
+        self.context_history = []
 
         with open("./prompts.json") as f:
             self.prompts = json.load(f)
 
-    def respond(self, message: str, image: str):
+    def respond(self, message: str, image_base64: str):
         completion = self.client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
@@ -27,17 +29,21 @@ class ChatService:
                 {
                     "role": "user",
                     "content": [
-                        {"type": "text", "text": "What's in this image?"},
+                        {"type": "text", "text": message},
                         {
                             "type": "image_url",
                             "image_url": {
-                                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                                "url": image_base64,
                             }
                         },
                     ],
                 }
             ],
         )
+
+        response = completion.choices[0].message.content
+        self.context_history.append(response)
+
 
 if __name__ == "__main__":
     chat = ChatService()
